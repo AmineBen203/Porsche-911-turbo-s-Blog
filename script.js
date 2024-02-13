6c4fddfc8ddddcadc89fe7752b3fe500dcce795b
AmineBen203 <121176837+AmineBen203@users.noreply.github.com>
2024-02-13 15:42:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("music-toggle").addEventListener("click", function() {
+    var musicPlayer = document.getElementById("music-player");
+    var arrowIcon = document.getElementById("arrow-icon");
+
+    if (musicPlayer.style.right === "-310px") { // If music player is hidden
+        musicPlayer.style.right = "0";
+        arrowIcon.style.transform = "rotate(0deg)"; // Rotate arrow to point right
+    } else {
+        musicPlayer.style.right = "-310px";
+        arrowIcon.style.transform = "rotate(180deg)"; // Rotate arrow to point left
+    }
+});
